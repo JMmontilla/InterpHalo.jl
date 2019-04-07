8026c79a4b36f9fe12556facf6934d1118d3605f
Jose Manuel <montilla_aero@protonmail.com>
2019-04-07 10:39:31 +0200

    Create the main program
    Now the H1 and H2 orbits can be interpolated

--- src/InterpHalo.jl
+++ src/InterpHalo.jl
@@ -1,5 +1,46 @@
+__precompile__()
+
 module InterpHalo
 
-greet() = print("Hello World!")
+using JLD, Interpolations
+
+export intH
+
+H1 = load("~/.julia/packages/InterpHalo/src/H1Data.jld")
+H2 = load("~/.julia/packages/InterpHalo/src/H2Data.jld")
+
+ORB1 = H1["ORB"]
+T1   = H1["T"]
+
+ORB2 = H2["ORB"]
+T2   = H2["T"]
+
+intORB1 = interpolate(ORB1, BSpline(Quadratic(Reflect(OnCell()))))
+intORB2 = interpolate(ORB2, BSpline(Quadratic(Reflect(OnCell()))))
+
+interpH1(x,y,z) = intORB1(x,y,z)
+interpH2(x,y,z) = intORB2(x,y,z)
+
+function intH(x::Real,orb::Real,family::Int)
+
+    if 0 > x || 1 < x
+        error("first argument 'x' must be 0<=x<=1")
+    elseif 0 > orb || 1 < orb
+        error("second argument 'orb' must be 0<=orb<=1")
+    end
+
+    if family == 1 # H1
+        i = 1 + x*(size(ORB1,1)-1)
+        j = 1 + x*(size(ORB1,3)-1)
+        return interpH1(i,2:7,j)
+    elseif family == 2 # H2
+        i = 1 + x*(size(ORB2,1)-1)
+        j = 1 + x*(size(ORB2,3)-1)
+        return interpH2(i,2:7,j)
+    else
+        error("last argument has to be either 1 (H1 family) or 2 (H2 family)")
+    end
+
+end
 
 end # module
